0c07a97b0456da442dd440bc20e060f4ae4e7d0d
UtkarshBhati <utkarsh.bhati@gmail.com>
2021-01-16 11:14:01 -0500

    Proxy test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cdbookstore';
+  title = 'CD Bookstore';
+  users;
+  
+
+  constructor(private httpClient: HttpClient){};
+
+  getTest2(){
+     
+      return this.httpClient.get('/api/v1/getUsers');
+      // .subscribe(()=>
+      //   console.log(res);
+      // );
+  }
 }
